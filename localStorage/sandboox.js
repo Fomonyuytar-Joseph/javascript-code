1f93043ddf1023c4d14552ef6ae2ceee061d04ea
Fomonyuytar Joseph <josephneymar33@gmail.com>
2021-08-16 21:28:37 -0700

    init

---
+++ localStorage/sandboox.js
@@ -0,0 +1,9 @@
+//storing data to local storage
+
+localStorage.setItem('name','Joseph');
+localStorage.setItem('age','50');
+
+//retrieving data
+ let name=localStorage.getItem('name');
+ let age=localStorage.getItem('age');
+ console.log(name,age);
