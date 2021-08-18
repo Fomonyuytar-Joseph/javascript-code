249ec14cd0dc73c4acdf92a92e70a6cef76658ac
Fomonyuytar Joseph <josephneymar33@gmail.com>
2021-08-17 21:22:06 -0700

    init

--- localStorage/sandboox.js
+++ localStorage/sandboox.js
@@ -1,9 +1,20 @@
 //storing data to local storage
 
-localStorage.setItem('name','Joseph');
-localStorage.setItem('age','50');
+// localStorage.setItem('name','Joseph');
+// localStorage.setItem('age','50');
 
-//retrieving data
- let name=localStorage.getItem('name');
- let age=localStorage.getItem('age');
- console.log(name,age);
+// //retrieving data
+//  let name=localStorage.getItem('name');
+//  let age=localStorage.getItem('age');
+//  console.log(name,age);
+
+//strigifying and paarsing data
+const todos=[{text:'play mariokart', author:'mario'},
+             { text:'feeling sleepy', author:'joseph'}];
+
+            //console.log(JSON.stringify(todos));
+            localStorage.setItem('todos',JSON.stringify(todos));//store elements in the local storage
+
+    const stored =localStorage.getItem('todos');
+    console.log(stored);
+              
